af4fbebf5a496d754b1d1de4c8f3bcbee063b4f0
hyeg0121 <hyeg0121@naver.com>
2023-03-31 22:11:19 +0900

    화살표를 누르면 figure가 오른쪽으로 이동

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ function moveLeft(){
     var el = document.querySelector("figure");
     el.style.transform = "translateX(-25vw)"
 }
+
+function moveRight(){
+    var el = document.querySelector("figure");
+    el.style.transform = "translateX(25vw)"
+}
