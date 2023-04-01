f30d623a0871c02ab9a93e6d0da39adad1b73cb3
hyeg0121 <hyeg0121@naver.com>
2023-04-02 07:54:57 +0900

    figure 움직임 부드럽게

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function moveLeft(){
     var el = document.querySelector("figure");
+    el.style.transition = "0.9s"
     el.style.transform = "translateX(-25vw)"
 }
 
 function moveRight(){
     var el = document.querySelector("figure");
+    el.style.transition = "0.9s"
     el.style.transform = "translateX(25vw)"
 }
