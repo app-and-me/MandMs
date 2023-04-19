dbbd545e982c1470cd492c44c9e64da07b6b5bf9
hyeg0121 <hyeg0121@naver.com>
2023-04-19 18:44:50 +0900

    버튼을 누르면 article 회전

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@ const frame = document.querySelector("section");
 const lists  = frame.querySelectorAll("article");
 const deg = 90;
 const len = lists.length-1;
+const btnLeft = document.querySelector(".btnLeft");
+const btnRight = document.querySelector(".btnRight");
+
 let i = 0;
+let num = 0;
 
 for (let element of lists){
     element.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
     i++;
 }
+
+btnLeft.addEventListener("click", ()=>{
+    num++;
+    frame.style.transform = `rotate(${deg * num}deg)`;
+});
+
+btnRight.addEventListener("click", ()=>{
+    num--;
+    frame.style.transform = `rotate(${deg * num}deg)`;
+});
