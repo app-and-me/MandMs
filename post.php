67c799f5e4bf44abb40d7dba647f0fdaf9256a08
parkseyeon06 <psyvitamin@gmail.com>
2023-05-16 23:04:30 +0900

    변경 사항 1(확정x)

--- post.php
+++ post.php
@@ -1,38 +1,25 @@
 <?php
-    // MySQL 데이터베이스 연결 정보
-    $servername = "localhost";  // MySQL 서버 주소
-    $username = "root";    // MySQL 사용자 이름
-    $password = "0000";    // MySQL 비밀번호
-    $dbname = "m&mz";  // 사용할 데이터베이스 이름
+// 데이터베이스 연결 설정
+$servername = "localhost";
+$username = "사용자이름";
+$password = "비밀번호";
+$dbname = "데이터베이스이름";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    // MySQL 데이터베이스에 연결
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// 사용자로부터 입력받은 데이터
+$title = $_POST['title'];
+$content = $_POST['content'];
 
-    // 연결 확인
-    if ($conn->connect_error) {
-        die("MySQL 연결 실패: " . $conn->connect_error);
-    }
-
-    // 연결 성공 메시지 출력
-    echo "MySQL 연결 성공";
-
-    // 글 업로드 처리
-    if ($_SERVER["post"] == "POST") {
-        // 제목과 내용 가져오기
-        $title = $_POST['title'];
-        $emotion = $_POST['emotion'];
-        $music = $_POST['music'];
-        $content = $_POST['content'];
-
-        // 제목과 내용을 데이터베이스에 삽입하기
-        $sql = "INSERT INTO post (title, emotion, music, content) VALUES ('$title', '$emotion', '$music', '$content')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "글이 성공적으로 업로드되었습니다.";
-        } else {
-            echo "글 업로드 오류: " . $conn->error;
-        }
-    }
+// SQL INSERT 문 실행
+$sql = "INSERT INTO posts (title, content) VALUES ('$title', '$content')";
+if ($conn->query($sql) === TRUE) {
+    echo "데이터가 성공적으로 삽입되었습니다.";
+} else {
+    echo "오류: " . $sql . "<br>" . $conn->error;
+}
 
 // 데이터베이스 연결 종료
 $conn->close();
