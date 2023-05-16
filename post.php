1dc436c5d2240227ef86c43ccde4a467d786e5cc
Hyunji Kim <papertoy01@gmail.com>
2023-05-16 22:34:38 +0900

    글 업로드 (오류류)

--- post.php
+++ post.php
@@ -16,6 +16,24 @@
     // 연결 성공 메시지 출력
     echo "MySQL 연결 성공";
 
-    // 연결 종료
-    $conn->close();
+    // 글 업로드 처리
+    if ($_SERVER["post"] == "POST") {
+        // 제목과 내용 가져오기
+        $title = $_POST['title'];
+        $emotion = $_POST['emotion'];
+        $music = $_POST['music'];
+        $content = $_POST['content'];
+
+        // 제목과 내용을 데이터베이스에 삽입하기
+        $sql = "INSERT INTO post (title, emotion, music, content) VALUES ('$title', '$emotion', '$music', '$content')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "글이 성공적으로 업로드되었습니다.";
+        } else {
+            echo "글 업로드 오류: " . $conn->error;
+        }
+    }
+
+// 데이터베이스 연결 종료
+$conn->close();
 ?>
