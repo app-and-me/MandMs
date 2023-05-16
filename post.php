a1b6d7afc9c3e0db5f619a6b5d6a911bf73c11a2
parkseyeon06 <psyvitamin@gmail.com>
2023-05-16 23:09:32 +0900

    연결 수정

--- post.php
+++ post.php
@@ -1,9 +1,9 @@
 <?php
 // 데이터베이스 연결 설정
 $servername = "localhost";
-$username = "사용자이름";
-$password = "비밀번호";
-$dbname = "데이터베이스이름";
+$username = "root";
+$password = "0000";
+$dbname = "posts";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
