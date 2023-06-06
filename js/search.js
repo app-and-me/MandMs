20c7c7a0e6016799d247638d9981c2b66fc196c1
hyeg0121 <hyeg0121@naver.com>
2023-06-06 21:28:36 +0900

    음악 재생 멈춤 버튼 구현

--- js/search.js
+++ js/search.js
@@ -1,6 +1,13 @@
 const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
 const audio = document.querySelectorAll("audio");
 
 play.addEventListener("click", e => {
+    pause.click();
     e.currentTarget.closest("article").querySelector("audio").play();
-})
+});
+
+pause.addEventListener("click", e => {
+    e.currentTarget.closest("article").querySelector("audio").pause();
+    e.currentTarget.closest("article").querySelector("audio").currentTime = 0;
+});
