a8389347374e922e5f498e90f8c7438314688aad
sunhee kim <iris_starry_@naver.com>
2023-06-08 09:51:35 +0900

    '끝내기'버튼 클릭 시 감정 선택 및 폼 제출 처리

--- js/write.js
+++ js/write.js
@@ -1,9 +1,21 @@
 const body = document.querySelector("body");
 const audio = document.querySelector("audio");
 
-
-function setBackground(){
+function setBackground() {
     let num = Math.floor(Math.random() * 5 + 1);
     console.log(num);
     body.style.backgroundImage = `url(img/article_bg/articlebg${num}.svg)`;
 }
+
+// "끝내기" 버튼 클릭 시 호출되는 함수
+document.getElementById("end").onclick = function() {
+     var selectedEmoji = document.querySelector('input[name="emoji"]:checked');
+    if (selectedEmoji) {
+        // 선택된 라디오 버튼의 값을 가져와서 $_POST['emotion']에 할당
+        document.getElementById("emotion").value = selectedEmoji.value;
+        document.querySelector("form").submit(); // 폼 제출
+    } else {
+        alert("감정을 선택해주세요.");
+    }
+};
+
