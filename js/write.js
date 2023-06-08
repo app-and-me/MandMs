0d9cd805125d4e7dedd94a4401096a0a1aa14b1b
hyeg <hyeg0121@naver.com>
2023-06-08 09:49:56 +0900

    버튼을 클릭했을 때 불투명도 조절

--- js/write.js
+++ js/write.js
@@ -1,9 +1,18 @@
 const body = document.querySelector("body");
 const audio = document.querySelector("audio");
-
+const emojiButtons = document.querySelectorAll(".emoji_button");
 
 function setBackground(){
     let num = Math.floor(Math.random() * 5 + 1);
     console.log(num);
     body.style.backgroundImage = `url(img/article_bg/articlebg${num}.svg)`;
+}
+
+for(let e of emojiButtons){
+    e.addEventListener("click", () => {
+        for(let btn of emojiButtons){
+            btn.style.opacity = 0.5;
+        }
+        e.style.opacity = 1;
+    });
 }
