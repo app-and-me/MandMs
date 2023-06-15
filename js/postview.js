29ed3fed3aaed03ab7bdb609aaa77bc0a56bf5b4
hyeg0121 <hyeg0121@naver.com>
2023-06-15 21:48:56 +0900

    27인치 모니터에 맞게 수정

--- js/postview.js
+++ js/postview.js
@@ -1,5 +1,11 @@
 function setBackground() {
     let num = Math.floor(Math.random() * 5 + 1);
     console.log(num);
+    const body = document.querySelector("body");
     body.style.backgroundImage = `url(img/article_bg/articlebg${num}.svg)`;
 }
+
+const prevPage = document.querySelector("#prev_page");
+prevPage.addEventListener("click", ()=>{
+    window.open("index.html", "_top");
+});
