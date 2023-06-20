6ac6558834e2cd98367394d0ce277413112144a1
sunhee kim <iris_starry_@naver.com>
2023-06-20 18:54:20 +0900

    id 값 가져와서 result_article 구성

--- js/write.js
+++ js/write.js
@@ -15,9 +15,40 @@ function setBackground() {
     let num = Math.floor(Math.random() * 5 + 1);
     console.log(num);
     body.style.backgroundImage = `url(img/article_bg/articlebg${num}.svg)`;
-
-    write_result();
 }
+
+// 현재 URL에서 쿼리 파라미터를 가져오기
+const urlParams = new URLSearchParams(window.location.search);
+
+// "id" 쿼리 파라미터의 값 가져오기
+const id = urlParams.get('id');
+
+// id 값 사용하기
+console.log("값 가져왔다 = "+id)
+
+
+//글쓰기 클릭하면 write.html이동 그냥이동AJAX아님
+//write.html에서 id값으로 write.php통해서 다른 데이터 비동기로 가져와서
+
+function write_php(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                document.getElementById('result_article').innerHTML = xhr.responseText;
+            } else {
+                console.log("write.php로 id를 보내는 데 실패했습니다.");
+            }
+        }
+    };
+    xhr.open("POST", "write.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var data = "id=" + encodeURIComponent(id);
+    xhr.send(data);
+}
+
+// write_php 함수 호출
+write_php(id);
 
 // "끝내기" 버튼 클릭 시 호출되는 함수
 document.addEventListener("DOMContentLoaded", () => {
@@ -35,18 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
-
-
-function write_result() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById('result_article').innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("POST", "write.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send();
-}
-
 
